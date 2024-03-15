85d06255967027472594cc0f2a88c07f44bcc325
vivsoftware <134121567+vivsoftware@users.noreply.github.com>
2024-03-15 16:51:14 +0530

    new

--- Components/Pages/Admin Dashboard/AllTabContain.jsx
+++ Components/Pages/Admin Dashboard/AllTabContain.jsx
@@ -1,54 +1,73 @@
+import axios from 'axios';
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
 import { Col, TabContent, TabPane } from 'reactstrap';
+import spring_boot_url from '../../../Utils/springApi';
 import BSMContain from './BSM Contain';
 import Dashboard from './Dashboard';
+import Marketplace from './Marketplace';
 import RFQContain from './RFQContain';
 import TenderContain from './TenderContain';
 import UserContain from './UserContain';
-import Marketplace from './Marketplace';
-
 
 const AllTabContain = ({ activeTab, userDe, sellers, buyers }) => {
   const [data, setData] = useState(false);
+  const router = useRouter();
+  const [options, setoptions] = useState(null);
+  const [user, setUser] = useState(null);
+  const [Rfq, setRfq] = useState(null);
+  const [Tender, setTender] = useState(null);
+
+  ///////////////////////////////get all tender and rfq details //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  useEffect(() => {
+    axios.get(`${spring_boot_url}api/userRfq`)
+      .then(resp => {
+        setRfq(resp.data);
+      });
+  }, []);
 
 
-  // const [open, setOpen] = React.useState(false);
-
-  // const router = useRouter();
-  // const handleClose = () => {
-  //   setOpen(false);
-  // }
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const style = {
-  //   position: 'absolute',
-  //   top: '50%',
-  //   left: '50%',
-  //   transform: 'translate(-50%, -50%)',
-  //   width: 400,
-  //   bgcolor: 'background.paper',
-  //   border: '4px solid #ff8400',
-  //   boxShadow: 24,
-  //   p: 4,
-  // };
+  useEffect(() => {
+    axios.get(`${spring_boot_url}api/tender`)
+      .then(resp => {
+        console.log(resp.data.json);
+        localStorage.setItem("data", JSON.stringify(resp.data));
+        setTender(resp.data);
+      });
+  }, []);
 
 
-  // if (activeTab === 5) {
-  //   setOpen(true);
-
-  //   // handleOpen()
-  //   //  router.push("/");
-  // }
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   return (
 
 
     <Col>
-
       <TabContent activeTab={activeTab}>
+
+        <TabPane className={`${activeTab === '0' ? 'active' : ''}`} tabId={0} >
+          <Dashboard activeTab={activeTab} userDe={userDe} sellers={sellers} buyers={buyers} />
+        </TabPane>
+        <TabPane className={`${activeTab === '2' ? 'active' : ''}`} tabId={2} >
+          <UserContain />
+        </TabPane>
+        <TabPane className={`${activeTab === '3' ? 'active' : ''}`} tabId={3} >
+          <RFQContain />
+        </TabPane>
+        <TabPane className={`${activeTab === '4' ? 'active' : ''}`} tabId={4} >
+          <TenderContain Tender={Tender} />
+        </TabPane>
+        <TabPane className={`${activeTab === '1' ? 'active' : ''}`} tabId={1} >
+          <BSMContain userDe={userDe} Tender={Tender} Rfq={Rfq} />
+        </TabPane>
+        <TabPane className={`${activeTab === '5' ? 'active' : ''}`} tabId={5} >
+          <Marketplace activeTab={activeTab} />
+          {/* <MarketplacePop/> */}
+        </TabPane>
+      </TabContent>
+      {/* <TabContent activeTab={activeTab}>
 
         <TabPane className={`${activeTab === '0' ? 'active' : ''}`} tabId={0} >
           <Dashboard activeTab={activeTab} userDe={userDe} sellers={sellers} buyers={buyers} />
@@ -56,20 +75,21 @@ const AllTabContain = ({ activeTab, userDe, sellers, buyers }) => {
         <TabPane className={`${activeTab === '1' ? 'active' : ''}`} tabId={1} >
           <UserContain />
         </TabPane>
-        <TabPane className={`${activeTab === '2' ? 'active' : ''}`} tabId={2} >
+        <TabPane className={`${activeTab === '1' ? 'active' : ''}`} tabId={1} >
+          <BSMContain userDe={userDe} Tender={Tender} Rfq={Rfq} />
+        </TabPane>
+
+        <TabPane className={`${activeTab === '3' ? 'active' : ''}`} tabId={3} >
           <RFQContain />
         </TabPane>
-        <TabPane className={`${activeTab === '3' ? 'active' : ''}`} tabId={3} >
-          <TenderContain />
-        </TabPane>
         <TabPane className={`${activeTab === '4' ? 'active' : ''}`} tabId={4} >
-          <BSMContain />
+          <TenderContain Tender={Tender} />
         </TabPane>
+
         <TabPane className={`${activeTab === '5' ? 'active' : ''}`} tabId={5} >
-         <Marketplace activeTab={activeTab} />
+          <Marketplace activeTab={activeTab} />
           {/* <MarketplacePop/> */}
-        </TabPane>
-      </TabContent>
+
     </Col>
 
 
